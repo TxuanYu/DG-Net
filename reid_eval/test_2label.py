cc697464add67a8758bb523b1b9e1f1d3e1ef74c
Txuan <hohahi0@gmail.com>
2021-03-25 15:00:52 +0800

    for my environment

--- reid_eval/test_2label.py
+++ reid_eval/test_2label.py
@@ -23,64 +23,6 @@ import scipy.io
 import yaml
 from reIDmodel import ft_net, ft_netAB, ft_net_dense, PCB, PCB_test
 
-######################################################################
-# Options
-# --------
-parser = argparse.ArgumentParser(description='Training')
-parser.add_argument('--gpu_ids',default='0', type=str,help='gpu_ids: e.g. 0  0,1,2  0,2')
-parser.add_argument('--which_epoch',default=90000, type=int, help='80000')
-parser.add_argument('--test_dir',default='../../Market/pytorch',type=str, help='./test_data')
-parser.add_argument('--name', default='test', type=str, help='save model path')
-parser.add_argument('--batchsize', default=80, type=int, help='batchsize')
-parser.add_argument('--use_dense', action='store_true', help='use densenet121' )
-parser.add_argument('--PCB', action='store_true', help='use PCB' )
-parser.add_argument('--multi', action='store_true', help='use multiple query' )
-
-opt = parser.parse_args()
-
-str_ids = opt.gpu_ids.split(',')
-which_epoch = opt.which_epoch
-name = opt.name
-test_dir = opt.test_dir
-
-gpu_ids = []
-for str_id in str_ids:
-    id = int(str_id)
-    if id >=0:
-        gpu_ids.append(id)
-
-# set gpu ids
-if len(gpu_ids)>0:
-    torch.cuda.set_device(gpu_ids[0])
-
-######################################################################
-# Load Data
-# ---------
-#
-# We will use torchvision and torch.utils.data packages for loading the
-# data.
-#
-data_transforms = transforms.Compose([
-        transforms.Resize((256,128), interpolation=3),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-])
-
-if opt.PCB:
-    data_transforms = transforms.Compose([
-        transforms.Resize((384,192), interpolation=3),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) 
-    ])
-
-
-data_dir = test_dir
-image_datasets = {x: datasets.ImageFolder( os.path.join(data_dir,x) ,data_transforms) for x in ['gallery','query','multi-query']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=opt.batchsize,
-                                             shuffle=False, num_workers=16) for x in ['gallery','query','multi-query']}
-
-class_names = image_datasets['query'].classes
-use_gpu = torch.cuda.is_available()
 
 ######################################################################
 # Load model
@@ -164,56 +106,120 @@ def get_id(img_path):
         camera_id.append(int(camera[0]))
     return camera_id, labels
 
-gallery_path = image_datasets['gallery'].imgs
-query_path = image_datasets['query'].imgs
-mquery_path = image_datasets['multi-query'].imgs
-
-gallery_cam,gallery_label = get_id(gallery_path)
-query_cam,query_label = get_id(query_path)
-mquery_cam,mquery_label = get_id(mquery_path)
-
-######################################################################
-# Load Collected data Trained model
-print('-------test-----------')
-
-###load config###
-config_path = os.path.join('../outputs',name,'config.yaml')
-with open(config_path, 'r') as stream:
-    config = yaml.load(stream)
-
-model_structure = ft_netAB(config['ID_class'], norm=config['norm_id'], stride=config['ID_stride'], pool=config['pool'])
-
-if opt.PCB:
-    model_structure = PCB(config['ID_class'])
-
-model = load_network(model_structure)
-
-# Remove the final fc layer and classifier layer
-model.model.fc = nn.Sequential()
-model.classifier1.classifier = nn.Sequential()
-model.classifier2.classifier = nn.Sequential()
-
-# Change to test mode
-model = model.eval()
-if use_gpu:
-    model = model.cuda()
-
-# Extract feature
-since = time.time()
-with torch.no_grad():
-    gallery_feature = extract_feature(model,dataloaders['gallery'])
-    query_feature = extract_feature(model,dataloaders['query'])
-    time_elapsed = time.time() - since
-    print('Extract features complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    if opt.multi:
-        mquery_feature = extract_feature(model,dataloaders['multi-query'])
+if __name__ == '__main__':
     
-# Save to Matlab for check
-result = {'gallery_f':gallery_feature.numpy(),'gallery_label':gallery_label,'gallery_cam':gallery_cam,'query_f':query_feature.numpy(),'query_label':query_label,'query_cam':query_cam}
-scipy.io.savemat('pytorch_result.mat',result)
-if opt.multi:
-    result = {'mquery_f':mquery_feature.numpy(),'mquery_label':mquery_label,'mquery_cam':mquery_cam}
-    scipy.io.savemat('multi_query.mat',result)
-
-os.system('python evaluate_gpu.py')
+    
+    ######################################################################
+    # Options
+    # --------
+    parser = argparse.ArgumentParser(description='Training')
+    parser.add_argument('--gpu_ids',default='0', type=str,help='gpu_ids: e.g. 0  0,1,2  0,2')
+    parser.add_argument('--which_epoch',default=100000, type=int, help='80000')
+    parser.add_argument('--test_dir',default='../../Market/pytorch',type=str, help='./test_data')
+    parser.add_argument('--name', default='E0.5new_reid0.5_w30000', type=str, help='save model path')
+    parser.add_argument('--batchsize', default=80, type=int, help='batchsize')
+    parser.add_argument('--use_dense', action='store_true', help='use densenet121' )
+    parser.add_argument('--PCB', action='store_true', help='use PCB' )
+    parser.add_argument('--multi', action='store_false', help='use multiple query' )
+    
+    opt = parser.parse_args()
+    
+    str_ids = opt.gpu_ids.split(',')
+    which_epoch = opt.which_epoch
+    name = opt.name
+    test_dir = opt.test_dir
+    
+    gpu_ids = []
+    for str_id in str_ids:
+        id = int(str_id)
+        if id >=0:
+            gpu_ids.append(id)
+    
+    # set gpu ids
+    if len(gpu_ids)>0:
+        torch.cuda.set_device(gpu_ids[0])
+    
+    ######################################################################
+    # Load Data
+    # ---------
+    #
+    # We will use torchvision and torch.utils.data packages for loading the
+    # data.
+    #
+    data_transforms = transforms.Compose([
+            transforms.Resize((256,128), interpolation=3),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    
+    if opt.PCB:
+        data_transforms = transforms.Compose([
+            transforms.Resize((384,192), interpolation=3),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) 
+        ])
+    
+    
+    data_dir = test_dir
+    image_datasets = {x: datasets.ImageFolder( os.path.join(data_dir,x) ,data_transforms) for x in ['gallery','query','multi-query']}
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=opt.batchsize,
+                                                 shuffle=False, num_workers=16) for x in ['gallery','query','multi-query']}
+    
+    class_names = image_datasets['query'].classes
+    use_gpu = torch.cuda.is_available()
+    
+    
+    
+    gallery_path = image_datasets['gallery'].imgs
+    query_path = image_datasets['query'].imgs
+    mquery_path = image_datasets['multi-query'].imgs
+    
+    gallery_cam,gallery_label = get_id(gallery_path)
+    query_cam,query_label = get_id(query_path)
+    mquery_cam,mquery_label = get_id(mquery_path)
+    
+    ######################################################################
+    # Load Collected data Trained model
+    print('-------test-----------')
+    
+    ###load config###
+    config_path = os.path.join('../outputs',name,'config.yaml')
+    with open(config_path, 'r') as stream:
+        config = yaml.load(stream)
+    
+    model_structure = ft_netAB(config['ID_class'], norm=config['norm_id'], stride=config['ID_stride'], pool=config['pool'])
+    
+    if opt.PCB:
+        model_structure = PCB(config['ID_class'])
+    
+    model = load_network(model_structure)
+    
+    # Remove the final fc layer and classifier layer
+    model.model.fc = nn.Sequential()
+    model.classifier1.classifier = nn.Sequential()
+    model.classifier2.classifier = nn.Sequential()
+    
+    # Change to test mode
+    model = model.eval()
+    if use_gpu:
+        model = model.cuda()
+    
+    # Extract feature
+    since = time.time()
+    with torch.no_grad():
+        gallery_feature = extract_feature(model,dataloaders['gallery'])
+        query_feature = extract_feature(model,dataloaders['query'])
+        time_elapsed = time.time() - since
+        print('Extract features complete in {:.0f}m {:.0f}s'.format(
+            time_elapsed // 60, time_elapsed % 60))
+        if opt.multi:
+            mquery_feature = extract_feature(model,dataloaders['multi-query'])
+        
+    # Save to Matlab for check
+    result = {'gallery_f':gallery_feature.numpy(),'gallery_label':gallery_label,'gallery_cam':gallery_cam,'query_f':query_feature.numpy(),'query_label':query_label,'query_cam':query_cam}
+    scipy.io.savemat('pytorch_result.mat',result)
+    if opt.multi:
+        result = {'mquery_f':mquery_feature.numpy(),'mquery_label':mquery_label,'mquery_cam':mquery_cam}
+        scipy.io.savemat('multi_query.mat',result)
+    
+    os.system('python evaluate_gpu.py')
